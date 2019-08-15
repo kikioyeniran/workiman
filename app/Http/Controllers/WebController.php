6785777051b6adf17c3fc0b3253d3836942cf90c
Babalola Macaulay <babsmacheda@ymail.com>
2019-08-15 20:28:11 +0100

    INitial COmmit

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -8,6 +8,7 @@ class WebController extends Controller
 {
     public function index()
     {
+        // dd(parse_url('mysql://b8dbb3182de3fe:fb018543@us-cdbr-iron-east-02.cleardb.net/heroku_4797e91'));
         return view('index');
     }
 }
