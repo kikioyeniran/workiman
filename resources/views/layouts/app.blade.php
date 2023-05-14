05e5fcf47c8a94b6d7e48fc936de8c57b43ad610
Kiki Oyeniran <kikioyeniran@gmail.com>
2023-05-14 08:28:41 +0100

    tawk.to updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -312,20 +312,20 @@
         })
     </script>
     <!--Start of Tawk.to Script-->
-    {{-- <script type="text/javascript">
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/61d63680f7cf527e84d0a3bd/1fomc9qag';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-        })();
-    </script> --}}
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/61d63680f7cf527e84d0a3bd/1fomc9qag';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
     <!--End of Tawk.to Script-->
 
-    <script  type="text/javascript">
+    {{-- <script  type="text/javascript">
         var config = {
           phone :" 234802 207 0606",
           call :"Chat with a support agent now!",
@@ -342,7 +342,7 @@
       
           s.onload = function () { tmWidgetInit(config) };
           var x = document.getElementsByTagName("script")[0]; x.parentNode.insertBefore(s, x);
-      </script>
+      </script> --}}
 
     <!-- Begin Inspectlet Asynchronous Code -->
     <script type="text/javascript">
